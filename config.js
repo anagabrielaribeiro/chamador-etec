311d48a65a1e3fe7732441ed2484ea220cc41174
Ana Gabriela <72254342+anagabrielaribeiro@users.noreply.github.com>
2026-01-18 20:26:16 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,23 @@
+// Substitua tudo abaixo pelo código que o Firebase te deu
+const firebaseConfig = {
+
+  apiKey: "AIzaSyBah5LjXxzoSayt3N0BjZXQvqnU0blS2jw",
+
+  authDomain: "painelsenha-9d916.firebaseapp.com",
+
+  databaseURL: "https://painelsenha-9d916-default-rtdb.firebaseio.com",
+
+  projectId: "painelsenha-9d916",
+
+  storageBucket: "painelsenha-9d916.firebasestorage.app",
+
+  messagingSenderId: "769541307537",
+
+  appId: "1:769541307537:web:6015b9301dac2b78631330"
+
+};
+
+
+// Inicializa o Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
